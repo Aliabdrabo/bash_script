b304dd2c6603354cc3af681173bac3e276fadde4
Ali <aliabdrabu78@gmail.com>
2024-08-11 01:24:41 +0300

    modifying file_and_dir.sh script

--- file_and_dir.sh
+++ file_and_dir.sh
@@ -1,161 +1,245 @@
 #!/bin/bash
 
+function exit_o(){
+
+        read -p "# Do you want to exit? (yes/no): " exit_choice
+        if [[ $exit_choice == "yes" ]]; then
+                  exit
+        elif [[ $exit_choice == "no" ]] ; then
+                  continue 2> /dev/null
+        else
+                  echo "invalid option exiting...."
+                  exit
+        fi
+}
+
+
+#function break_o(){
+
+ #       read -p "# Do you want to exit this process ? (yes/no): " exit_choice
+  #      if [[ $exit_choice == "yes" ]]; then
+   #               break 2> /dev/null 
+    #    elif [[ $exit_choice == "no" ]] ; then
+                 
+                 # echo "invalid option exiting...."
+     #             continue 2> /dev/null
+#	else 
+#		echo"invalid exiting ...."
+#		break
+
+#        fi
+#}
+
+
 function create_file() {
-        read -p " please enter the path of the file : " path
-	touch "$path"
-	#check if the previous command execute
-	if [ $? -eq 0 ]; then
-		echo "file is created successfully"
-	else
-		echo "erro creating file please try again "
-		create_file
-	fi
+	while true ;
+	do
+		
+                read -p " please enter the path of the file : " path
+	        touch "$path"
+	        #check if the previous command execute
+	        if [ $? -eq 0 ] 2> /dev/null; then
+		       echo -e "<<<<file is created successfully>>>>\n"
+		       echo -e "*********************************************************************\n\n"
+		       break
+	        else
+		        echo "erro creating file please try again "
+			continue
+	        fi
+		
+	done	
 }
 
 #**********************************************************************************************************************
 function create_dir(){
-        read -p " please enter the path of the directory : " path
-        mkdir "$path"
-        if [ $? -eq 0 ]; then
-                echo "file is created successfully"
-        else    
-                echo "erro creating directory please try again"
-		create_dir
-        fi
+	while true;
+	do
+
+                read -p " please enter the path of the directory : " path
+	        echo -e "*****************************************************************************\n\n"
+                mkdir "$path"
+                if [ $? -eq 0 ] 2> /dev/null ; then
+                         echo -e "<<<<file is created successfully>>>>\n"
+		         echo -e"*****************************************************************************\n\n"
+		         break
+                else    
+                         echo "erro creating directory please try again"
+		         continue
+                fi
+	done	
 }
 
 #***********************************************************************************************************************
 function copy() {
-	read -p "what do you want to copy directory / file ? " type
-	read -p "please enter the source path of the file or the dir you want to copy : " source
-	read -p "please enter the destination path of the file or the dir you want to move : " destination
-	if [ -f "$source" ] && [ -d"destination" ]; then
-		read -p "please enter the destination path of the file or the dir you want to copy : " destination
-                cp -r "$source" "$destination"
-                if [ $? -eq 0 ]; then
-                         echo "you copied the $type sucessfully"
+	while true;
+	do
+
+	        read -p "what do you want to copy directory / file ? " type
+	        read -p "please enter the source path of the file or the dir you want to copy : " source
+	        read -p "please enter the destination path of the file or the dir you want to move : " destination
+	        if [ -f "$source" ] && [ -d"destination" ]; then
+                           cp -r "$source" "$destination"
+                           if [ $? -eq 0 ] 2> /dev/null; then
+                                   echo "you copied the $type sucessfully"
+				   break
+                           else
+                                   echo " there's something wrong happend please try again"
+                                   continue
+                           fi
                 else
-                         echo " there's something wrong happend please try again"
-                         copy
-                fi
-        else
-		echo "your source or destination may not exisit please check the right one"
-		copy
-	fi	
+		           echo "your source or destination may not exisit please check the right one"
+		           continue
+	        fi
+         done
 }
 
 #************************************************************************************************************************
 function move() {
-        read -p "what do you want to copy directory / file ? " type
-        read -p "please enter the source path of the file or the dir you want to move : " source
-       	read -p "please enter the destination path of the file or the dir you want to move : " destination
+	while true;
+	do
+
+                read -p "what do you want to move directory / file ? " type
+                read -p "please enter the source path of the file or the dir you want to move : " source
+       	        read -p "please enter the destination path of the file or the dir you want to move : " destination
         
-	if [ -f "$source" ] || [ -d "$source" ] && [ -d "$destination" ]; then
-		chmod +rw "$source"
+	        if [ -f "$source" ] || [ -d "$source" ] && [ -d "$destination" ]; then
+		        chmod +rw "$source"
 
-                # -i:to prompt for confirmation befor confirmation
-                 mv -i "$source" "$destination"
+                        # -i:to prompt for confirmation befor confirmation
+                        mv -i "$source" "$destination"
 
-                  if [ $? -eq 0 ]; then
-                            echo "you moved the $type sucessfully"
-                  else
-                             echo " there's something wrong happend please try again"
-                             move
-                  fi
+                        if [ $? -eq 0 ] 2> /dev/null; then
+                                 echo "you moved the $type sucessfully"
+				 break
+                        else
+                                 echo " there's something wrong happend please try again"
+                                 continue
+                        fi
 		
-        else
-                 echo "your source or destination may not exist please check the right one"
-		 move
-        fi 
+                 else
+                        echo "your source or destination may not exist please check the right one"
+		        continue
+                 fi
+	done	 
 }
 
 #************************************************************************************************************************
 function rename() {
+	while true;
+	do
 
-        read -p "please enter the old name of the file or the dir you want to rename : " old_name
-        read -p "please enter the new_name of the file or the dir  : " new_name
+
+                read -p "please enter the old name of the file or the dir you want to rename : " old_name
+                read -p "please enter the new_name of the file or the dir  : " new_name
 
 
                 # -i:to prompt for confirmation befor overwrite any file with the same new_name 
                  mv -i "$old_name" "$new_name"
 
-                  if [ $? -eq 0 ]; then
+                  if [ $? -eq 0 ] 2> /dev/null; then
                             echo "you renamed the $old_name sucessfully to $new_name"
+			    break
                   else
                              echo " there's something wrong happend please try again"
-                             rename
+                             continue
                   fi
+	done	  
 }
 
 #*******************************************************************************************************
 
 function delete(){
-	read -p "please enter the type directory/file : " type
-	read -p "please enter name of the $type : " name
-	read -p " are you sure you want to delet this $type y/n : " conf
-	if [ $conf = "y" ]; then
+	while true;
+	do
 
-		if [ -x "$(dirname "$name")" ]; then
+	       read -p "please enter the type directory/file : " type
+	       echo -e "***************************************************\n"
 
-			if [ $type = "file" ]; then
-				rm "$name"
-				#check if the previous command execute
-				if [ $? -eq 0 ]; then
-					echo "this $type i deleted successfully"
-				else
-					echo "there may be a problem please try again"
-					delete
-				fi 
+	       read -p "please enter name of the $type : " name
+	       echo -e "***************************************************\n"
 
-			elif [$type = "directory"]; then
+	       read -p " are you sure you want to delet this $type y/n : " conf
+	       echo -e "***************************************************\n"
 
-				rmdir "$name"
-				 if [ $? -eq 0 ]; then
-                                        echo "this $type i deleted successfully"
-                                else
-                                        echo "there may be a problem please try again"
-                                        delete
-                                fi
-			fi
+	       if [[  ${conf,,} = "y" ]]; then
 
-		else 
-		 	echo " you don't have the permission to delete this $type"
-		fi
-	else
-		break
-	fi	
+		       if [[ -x "$(dirname "$name")" ]]; then
+
+			        if [[ ${type,,} = "file" ]]; then
+				       rm "$name"
+				       #check if the previous command execute
+				       if [ $? -eq 0 ] 2> /dev/null; then
+					       echo "this $type i deleted successfully"
+					       break
+				       else
+					       echo "there may be a problem please try again"
+					       continue
+				       fi 
+
+			         elif [[ ${type,,} = "directory" ]]; then
+
+				       rmdir "$name"
+				       if [ $? -eq 0 ] 2> /dev/null; then
+                                               echo "this $type i deleted successfully"
+					       break
+                                        else
+                                               echo "there may be a problem please try again"
+                                               continue
+                                        fi
+			         fi
+
+		        else 
+		 	         echo " you don't have the permission to delete this $type"
+			         break
+		        fi
+
+	       else
+		        break
+	       fi
+	done       
 }
 
 
 #//////////////////////////////////////////////////////////////////////////////////////////////////////
 # main menu
+while true;
+do
 
-echo "welcom to file & dir mangment"
-echo " which of these option do you want to use? \n create_file \n create_dir \n copy \n move \n rename \n delete \n" 
-read option
+       echo "<<<< welcom to file & dir mangment >>>>"
+       echo -e "********************************************************************************************\n"
+       echo -e "please enter the option by it's number  to use it :- \n [1] create_file. \n [2] create_dir. \n [3] copy. \n [4] move. \n [5] rename. \n [6] delete. \n >>>>> :" 
+       read option
 
-case $option in
-	"create_file")
-		create_file
-		;;
-	"create_dir")
-		create_dir
-		;;
-	"copy")
-		copy
-		;;
-	"move")
-		move
-		;;
-	"rename")
-		rename
-		;;
-	"delete")
-		delete
-		;;
-	*)
-		echo "invalid option please try again"
-		;;
-esac
+       case $option in
+	       "1")
+		         create_file
+		         exit_o
+		         ;;
+	        "2")
+		         create_dir
+		         exit_o
+		         ;;
+	        "3")
+		         copy
+		         exit_o
+		         ;;
+	        "4")
+		         move
+		         exit_o
+		         ;;
+	        "5")
+		         rename
+		         exit_o
+		         ;;
+	        "6")
+		         delete
+		         exit_o
+		         ;;
+	        *)
+		         echo "invalid option please try again"
+		         continue
+		         ;;
+        esac
+done	
 
 #/////////////////////////////////////////////////////////////////////////////////////////////////////
