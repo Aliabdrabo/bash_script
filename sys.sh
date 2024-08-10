b9cdf902f50f43d09ce097eb98c4e0ec0b7fdcbf
Ali <aliabdrabu78@gmail.com>
2024-08-10 17:16:55 +0300

    modify sys,sh script

--- sys.sh
+++ sys.sh
@@ -21,7 +21,7 @@ function basic(){
 
 
 function cpu(){
-	echo -e "\n<<<<< this function will display the Cpu usage percentage for all processes of th system >>>>>\n"
+	echo -e "\n<<<<<  displaying the Cpu usage percentage for all processes of th system >>>>>\n"
 	echo    "*******************************************************************************************"
 	read -p "enter number of batches you want to display : " batch
 
@@ -40,7 +40,7 @@ function cpu(){
 
 
 function memory(){
-	echo -e "\n<<<<< this function will display the memory usage percentage for  processes of th system >>>>>\n"
+	echo -e "\n<<<<<  displaying the memory usage percentage for  processes of th system >>>>>\n"
         echo -e "******************************************************************************************************\n"
 	read -p "enter number of batches you want to display : " batch
         echo -e "<<<  pid   ,  user ,  Cpu%  >>>\e"
@@ -55,7 +55,7 @@ function memory(){
 
 
 function disk_space(){
-	echo -e "\n<<<<< this function display the total disk space, used space, and available space for each mounted filesystem >>>>>\n"
+	echo -e "\n<<<<<  displaying the total disk space, used space, and available space for each mounted filesystem >>>>>\n"
         echo -e "******************************************************************************************************\n"
         df -ah
 
@@ -64,14 +64,83 @@ function disk_space(){
 
 
 
-
+# report of system
 
 function report(){
 
+	hostname=$(hostname)
+	username=$(whoami)
+	date=$(date +"%A ,%B %D ,%Y %H:%M:%S")
+
+	echo -e "*************************************************************************************************************\n" 
+	echo -e "hostname is : $hostname \nuser : $username . \ndate: $date\n"  
+	echo -e "*************************************************************************************************************\n" 
+	
+	cpu_usage=$(top -bn1 | grep "Cpu(s):" | awk '{print $2 + $4}')
+	mem_usage=$(free -m | grep "Mem:" | awk '{print $3/$2 * 100.0}')
+        disk_usage=$(df -h / | grep "/$" | awk '{print $5}')
+	free_space=$(free -m | grep "Mem:" | awk '{print ($2-$3)/$2 * 100.0}')
+
+	echo -e "-Cpu usage : $cpu_usage \n-memory usage : $mem_usage \n-disk usage : $disk_usage \n-free space : $free_space\n " 
+	echo -e "*************************************************************************************************************\n" 
+
+	echo -e "memory usage in details (inmegabytes):\n$(free -m) \n\n"  
+	echo -e "*************************************************************************************************************\n"
+	echo -e"cpu usage in details :  $(cpu) "
+	echo -e "*************************************************************************************************************\n"
 
 
+}	
 
+function exit_o(){
+	read -p "# Do you want to exit? (yes/no): " exit_choice
+        if [[ $exit_choice == "yes" ]]; then
+                 exit
+	elif [[ $exit_choice == "no" ]]; then
+	         continue
+	else
+		echo "<<<<< invalid choice >>>>>"
+		echo "exitig"
+		exit
+        fi
+}
 
+#main##################################################################################
 
+while true;
+do
 
+           echo -e "-please choose option by number :-\n[1] basic system information. \n[2] Cpu usage mentoring. \n[3] memory usage mentoring. \n[4] Disk space mentoring \n[5] logging and reporting."
+           
+	   read  number
 
+           case $number in 
+        
+	           "1")
+	                  basic
+			  exit_o
+	                  ;;
+	           "2")
+	                  cpu
+			  exit_o
+	                  ;;
+	           "3")
+	                  memory
+			  exit_o
+	                  ;;
+	           "4")
+	                  disk_space
+			  exit_o
+	                  ;;
+	           "5")
+	                  report
+			  exit_o
+	                  ;;
+	           *)
+	                  echo -e "\n<<<<invalid option>>>>"
+			  echo -e "******************************************************************"
+	                  echo -e "\n<<<<<<<<<<<<please try again>>>>>>>>>>>>>"
+	                  exit_o
+	                  ;;
+           esac	      
+done
