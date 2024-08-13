d63430be1ac45cb4a65f5cbe03cb04ebd8182585
Ali <aliabdrabu78@gmail.com>
2024-08-13 18:49:06 +0300

    updated

--- sys.sh
+++ sys.sh
@@ -4,6 +4,10 @@ function basic(){
         echo -e "\n<<<<< this function will display you a basic information of operating system >>>>>\n"
         echo    "******************************************************************************************"
 
+	echo -e "<<operating system>>\n***************************************************"
+	uname 
+
+	echo -e "\n"
         echo -e "<<hostname>>\n*****************************************************"
         hostname
 
@@ -18,52 +22,49 @@ function basic(){
 }
 
 
-
+#######################################################################################
 
 function cpu(){
-	echo -e "\n<<<<<  displaying the Cpu usage percentage for all processes of th system >>>>>\n"
-	echo    "*******************************************************************************************"
-	read -p "enter number of batches you want to display : " batch
-
-
-
-	echo "<<<  pid   ,  user ,  Cpu%  >>>"
-	echo "*****************************************************"
-	top -bn "$batch" | awk 'NR<=50{print $1,$2,$9}'
-	echo -e "\n****************************************************************************************"
+	echo -e "\n<<<<<  displaying the Cpu usage percentage of all processes of th system >>>>>\n"
+	echo    "*************************************************************************************"
+        #calculate user & sys cpu%
+	cpu_usage=$(top -bn1 | grep "Cpu(s):" | awk '{print $2 + $4}')
+	echo -e "cpu usage percentage -->>( $cpu_usage )"
+	echo -e "\n***********************************************************************************"
 }
 
 
 
 
-
+#######################################################################################
 
 
 function memory(){
 	echo -e "\n<<<<<  displaying the memory usage percentage for  processes of th system >>>>>\n"
-        echo -e "******************************************************************************************************\n"
-	read -p "enter number of batches you want to display : " batch
-        echo -e "<<<  pid   ,  user ,  Cpu%  >>>\e"
-        echo "**********************************************************"
-        top -bn "$batch" | awk 'NR<=50{print $1,$2,$10}'
+        echo -e "******************************************************************************************************\n\n"
+	mem_usage=$(free -m | grep "Mem:" | awk '{print $3/$2 * 100.0}')
+        echo " Memory usage percentage -->> $mem_usage"
 	echo -e "\n*******************************************************"
 
 
 }
 
 
-
+#######################################################################################
 
 function disk_space(){
 	echo -e "\n<<<<<  displaying the total disk space, used space, and available space for each mounted filesystem >>>>>\n"
         echo -e "******************************************************************************************************\n"
-        df -ah
-
+        disk_usage=$(df -h / | grep "/$" | awk '{print $5}')
+        free_space=$(free -m | grep "Mem:" | awk '{print ($2-$3)/$2 * 100.0}')
+	echo -e "-disk usage : $disk_usage \n-free space : $free_space\n "
+	echo -e "\n\n"
+	df -ah
 	echo -e "\n*******************************************************************************************************\n"
 }
 
 
-
+#######################################################################################
 # report of system
 
 function report(){
@@ -75,29 +76,44 @@ function report(){
 	echo -e "*************************************************************************************************************\n" 
 	echo -e "hostname is : $hostname \nuser : $username . \ndate: $date\n"  
 	echo -e "*************************************************************************************************************\n" 
-	
-	cpu_usage=$(top -bn1 | grep "Cpu(s):" | awk '{print $2 + $4}')
+
 	mem_usage=$(free -m | grep "Mem:" | awk '{print $3/$2 * 100.0}')
         disk_usage=$(df -h / | grep "/$" | awk '{print $5}')
 	free_space=$(free -m | grep "Mem:" | awk '{print ($2-$3)/$2 * 100.0}')
+	swap_mem=$(free -m | awk '/Swap/ { print $3 / $2 * 100 }')
 
-	echo -e "-Cpu usage : $cpu_usage \n-memory usage : $mem_usage \n-disk usage : $disk_usage \n-free space : $free_space\n " 
+
+	echo -e "<<<<< Memory >>>>>"
 	echo -e "*************************************************************************************************************\n" 
+        echo -e "-memory usage : $mem_usage \n-swap memory : $swap_mem\n-disk usage : $disk_usage \n-free space : $free_space\n "
+	echo -e "memory usage in details (in megabytes):\n$(free -m) \n\n" 
 
-	echo -e "memory usage in details (inmegabytes):\n$(free -m) \n\n"  
+	read -p "enter number of batches (of processes) you want to display : " batch
+	echo -e "*********************************************************\n"
+	echo -e "Memory usage in details for processes : \n"
+	top -bn "$batch" | awk 'NR<=50{print $1,$2,$10}'
 	echo -e "*************************************************************************************************************\n"
-	echo -e"cpu usage in details :  $(cpu) "
+
+	echo -e "$(cpu)"
+	echo -e "number of cores --> $(nproc --all)"
+        echo -e "*********************************************************\n"
+	read -p "enter number of batches (of processes) you want to display : " batch
+	echo -e "*********************************************************\n"
+	echo -e "cpu usage in details : \n"
+	top -bn "$batch" | awk 'NR<=50{print $1,$2,$9}'
 	echo -e "*************************************************************************************************************\n"
 
 
 }	
+
+#######################################################################################
 
 function exit_o(){
 	read -p "# Do you want to exit? (yes/no): " exit_choice
         if [[ $exit_choice == "yes" ]]; then
                  exit
 	elif [[ $exit_choice == "no" ]]; then
-	         continue
+	         continue 2> /dev/null
 	else
 		echo "<<<<< invalid choice >>>>>"
 		echo "exitig"
@@ -137,10 +153,9 @@ do
 			  exit_o
 	                  ;;
 	           *)
-	                  echo -e "\n<<<<invalid option>>>>"
-			  echo -e "******************************************************************"
-	                  echo -e "\n<<<<<<<<<<<<please try again>>>>>>>>>>>>>"
+	                  echo -e "\n<<<< invalid option please try again >>>>"
 	                  exit_o
 	                  ;;
-           esac	      
+           esac	 
+	   echo -e "\n\n"
 done
